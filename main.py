55e6d8e27f553f7f8aba16e539c429121c400374
e13q <110967581+e13q@users.noreply.github.com>
2024-10-26 06:17:26 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import logging
+import os
 
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 from environs import Env
+from google.cloud import dialogflow
+
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -21,7 +25,19 @@ logger = logging.getLogger(__name__)
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path('newagent-pyvi', '1927233901')
+
+    text_input = dialogflow.TextInput(text=update.message.text, language_code='RU')
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+        
+    await update.message.reply_text(str(response.query_result.fulfillment_text))
 
 
 def main():
@@ -30,6 +46,7 @@ def main():
     application = Application.builder().token(
         env.str('TELEGRAM_MAIN_BOT_TOKEN')
     ).build()
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = env.str("GOOGLE_APPLICATION_CREDENTIALS")
     application.add_handler(
         MessageHandler(filters.TEXT, echo)
     )
