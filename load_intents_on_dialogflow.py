730a6849834bdab38a2ea94d1f64ff3622b9b742
e13q <110967581+e13q@users.noreply.github.com>
2024-10-29 22:32:49 +0300

    remove unusable funcs and move cli to main

--- load_intents_on_dialogflow.py
+++ load_intents_on_dialogflow.py
@@ -22,27 +22,14 @@ def load_intents(intents: list, dataflow_project_id):
             training_phrases.append(training_phrase)
         text = dialogflow.Intent.Message.Text(text=[intent['answer']])
         message = dialogflow.Intent.Message(text=text)
-
         intent = dialogflow.Intent(
             display_name=name,
             training_phrases=training_phrases,
             messages=[message]
         )
-        response = intents_client.create_intent(
+        intents_client.create_intent(
             request={"parent": parent, "intent": intent}
         )
-        print("Intent created: {}".format(response))
-
-
-def request_json(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def load_json(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return json.load(file)
 
 
 def load_parser():
@@ -73,11 +60,16 @@ if __name__ == '__main__':
         "GOOGLE_APPLICATION_CREDENTIALS"
     )
     dataflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
-    if url:
-        intents = request_json(url)
-    elif path:
-        intents = load_json(path)
+    intents = None
+    if url or path:
+        if url:
+            response = requests.get(url)
+            response.raise_for_status()
+            intents = response.json()
+        elif path:
+            with open(path, "r", encoding="utf-8") as file:
+                intents = json.load(file)
+        load_intents(intents, dataflow_project_id)
+        print("Intents created")
     else:
         print('Specify path to data file using one of arguments: --url, --path') # noqa
-        exit()
-    load_intents(intents, dataflow_project_id)
